f5c51d7fa8888dcf9f9826e685099d1ab573fc84
Hakim <hakimblhya@gmail.com>
2016-02-11 19:19:23 +0100

    object

--- turfu.py
+++ turfu.py
@@ -4,21 +4,42 @@ class Warehouse:
 
   
 class ProductType:
-  def __init__(self):
-    pass
+  def __init__(self, _id, weight):
+    self._id = _id;
+    self.weight = weight    
+  def getWeight():
+    return self.weight
+  def setWeight(weight):
+    self.weight = weight
 
+class Product:
+  def __init__(self, ptype):
+    self.ptype = ptype;
 class Order:
   def __init__(self):
     pass
 
 
+class Cellule:
+  def __init__(self):
+    pass
+  
 class Map:
   def __init__(self, R, C):
     self.R = R
     self.C = C
-    self.matrix = [[None for i in range(C)] for i in range(R)]
+    self.matrix = [[Cellule() for i in range(C)] for i in range(R)]
     print(self.matrix)
-
-
+  
+class Parser:
+  def __init__(self, fileName):
+    self.f = open(fileName)
+    self.lines = self.f.readlines()
+    print(self.lines)
+    self.close()
+    
 if __name__ == "__main__":
-  t = Map(2, 3);
+  
+
+  
+  
