06423a9860462f1a2671c39eaa76671879dc22a3
Hakim <hakimblhya@gmail.com>
2016-02-11 19:07:37 +0100

    object

--- turfu.py
+++ turfu.py
@@ -1,3 +1,24 @@
+class Warehouse:
+  def __init__(self):
+    pass
+
+  
+class ProductType:
+  def __init__(self):
+    pass
+
+class Order:
+  def __init__(self):
+    pass
+
+
+class Map:
+  def __init__(self, R, C):
+    self.R = R
+    self.C = C
+    self.matrix = [[None for i in range(C)] for i in range(R)]
+    print(self.matrix)
+
+
 if __name__ == "__main__":
-  print("#TeamTurfu")
-  print("Hello")
+  t = Map(2, 3);
